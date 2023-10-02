80edec4745c2da2847df55d1e10c382811060044
bouhaa <boukehaarsma23@gmail.com>
2023-10-02 11:39:35 +0200

    re-enabled uploaded tests, also add logic and test to handle non existing stable channels

--- test/run.sh
+++ test/run.sh
@@ -22,24 +22,30 @@ check() {
 echo
 echo '== get_img_url'
 echo '==== stable channel'
-check 'should select the system image asset in stable'           $(cat test1.dat | get_img_url stable) 'chimeraos-1_0000000.img.tar.xz'
-check 'should prioritize latest stable asset'                    $(cat test2.dat | get_img_url stable) 'chimeraos-1_0000004.img.tar.xz'
-check 'should prioritize stable asset over newer testing asset'  $(cat test2.dat | get_img_url stable) 'chimeraos-1_0000004.img.tar.xz'
-check 'should prioritize stable asset over newer unstable asset' $(cat test2.dat | get_img_url stable) 'chimeraos-1_0000004.img.tar.xz'
+check 'should select the system image asset in stable'            $(cat test1.dat  | get_img_url stable) 'chimeraos-1_0000000.img.tar.xz'
+check 'should prioritize latest stable asset'                     $(cat test2.dat  | get_img_url stable) 'chimeraos-1_0000004.img.tar.xz'
+check 'should prioritize stable asset over newer testing asset'   $(cat test2.dat  | get_img_url stable) 'chimeraos-1_0000004.img.tar.xz'
+check 'should prioritize stable asset over newer unstable asset'  $(cat test2.dat  | get_img_url stable) 'chimeraos-1_0000004.img.tar.xz'
+check 'should prioritize uploaded stable asset over not-uploaded' $(cat test3a.dat | get_img_url stable) 'chimeraos-1_0000001.img.tar.xz'
+check 'should handle non existing stable channel'                 $(cat test6.dat  | get_img_url stable) ''
 
 echo
 echo '==== testing channel'
-check 'should select the system image asset in testing'          $(cat test1.dat | get_img_url testing) 'chimeraos-2_0000000.img.tar.xz'
-check 'should prioritize stable asset over older testing asset'  $(cat test2.dat | get_img_url testing) 'chimeraos-1_0000004.img.tar.xz'
-check 'should prioritize stable asset over newer unstable asset' $(cat test2.dat | get_img_url testing) 'chimeraos-1_0000004.img.tar.xz'
-check 'should return stable when no testing asset exists'        $(cat test5.dat | get_img_url testing) 'chimeraos-1_0000000.img.tar.xz'
+check 'should select the system image asset in testing'            $(cat test1.dat  | get_img_url testing) 'chimeraos-2_0000000.img.tar.xz'
+check 'should prioritize stable asset over older testing asset'    $(cat test2.dat  | get_img_url testing) 'chimeraos-1_0000004.img.tar.xz'
+check 'should prioritize stable asset over newer unstable asset'   $(cat test2.dat  | get_img_url testing) 'chimeraos-1_0000004.img.tar.xz'
+check 'should return stable when no testing asset exists'          $(cat test5.dat  | get_img_url testing) 'chimeraos-1_0000000.img.tar.xz'
+check 'should prioritize uploaded testing asset over not-uploaded' $(cat test3b.dat | get_img_url testing) 'chimeraos-2_0000001.img.tar.xz'
+check 'should handle non existing stable channel'                  $(cat test6.dat  | get_img_url testing) 'chimeraos-1_0000001.img.tar.xz'
 
 echo
 echo '==== unstable channel'
-check 'should select the system image asset in unstable'         $(cat test1.dat | get_img_url unstable) 'chimeraos-3_0000000.img.tar.xz'
-check 'should prioritize stable asset over older testing asset'  $(cat test2.dat | get_img_url unstable) 'chimeraos-1_0000004.img.tar.xz'
-check 'should prioritize stable asset over older unstable asset' $(cat test2.dat | get_img_url unstable) 'chimeraos-1_0000004.img.tar.xz'
-check 'should return stable when no unstable asset exists'       $(cat test5.dat | get_img_url unstable) 'chimeraos-1_0000000.img.tar.xz'
+check 'should select the system image asset in unstable'            $(cat test1.dat  | get_img_url unstable) 'chimeraos-3_0000000.img.tar.xz'
+check 'should prioritize stable asset over older testing asset'     $(cat test2.dat  | get_img_url unstable) 'chimeraos-1_0000004.img.tar.xz'
+check 'should prioritize stable asset over older unstable asset'    $(cat test2.dat  | get_img_url unstable) 'chimeraos-1_0000004.img.tar.xz'
+check 'should return stable when no unstable asset exists'          $(cat test5.dat  | get_img_url unstable) 'chimeraos-1_0000000.img.tar.xz'
+check 'should prioritize uploaded unstable asset over not-uploaded' $(cat test3c.dat | get_img_url unstable) 'chimeraos-2_0000001.img.tar.xz'
+check 'should handle non existing stable channel'                   $(cat test6.dat  | get_img_url unstable) 'chimeraos-2_0000001.img.tar.xz'
 
 echo
 echo '==== direct'
